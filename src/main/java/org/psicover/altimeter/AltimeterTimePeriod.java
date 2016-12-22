b9060af69b346b1537466363a1548a8a18db48fa
olopes <psicover.dev@gmail.com>
2016-12-22 08:35:22 +0000

    Time scale
    Preliminar "zoom" - visible time range currently set to 100 seconds.
    Chart "navigation" using a slider.

---
+++ src/main/java/org/psicover/altimeter/AltimeterTimePeriod.java
@@ -0,0 +1,76 @@
+package org.psicover.altimeter;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import org.jfree.data.time.RegularTimePeriod;
+
+public class AltimeterTimePeriod extends RegularTimePeriod implements Serializable {
+	private static final long serialVersionUID = -6349245393675620158L;
+	private final long increment;
+	private final long time;
+	
+	public AltimeterTimePeriod(SampleRate rate) {
+		this(1000L/rate.samplesPerSecond(), 0);
+	}
+	
+	private AltimeterTimePeriod(long increment, long time) {
+		this.increment = increment;
+		this.time = time;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if(o instanceof AltimeterTimePeriod) {
+			long otime = ((AltimeterTimePeriod)o).time;
+			if(time > otime) return 1;
+			else if (time == otime) return 0;
+			else return -1;
+		}
+		return 0;
+	}
+
+	@Override
+	public RegularTimePeriod previous() {
+		if(this.time == 0)
+			return null;
+		return new AltimeterTimePeriod(this.increment, this.time-this.increment);
+	}
+
+	@Override
+	public RegularTimePeriod next() {
+		if(this.time+this.increment >= Integer.MAX_VALUE)
+			return null;
+		return new AltimeterTimePeriod(this.increment, this.time+this.increment);
+	}
+
+	@Override
+	public long getSerialIndex() {
+		return this.time;
+	}
+
+	@Override
+	public void peg(Calendar calendar) {
+		// do nothing?
+	}
+
+	@Override
+	public long getFirstMillisecond() {
+		return this.time;
+	}
+
+	@Override
+	public long getFirstMillisecond(Calendar calendar) {
+		return getFirstMillisecond();
+	}
+
+	@Override
+	public long getLastMillisecond() {
+		return this.time+this.increment-1L;
+	}
+
+	@Override
+	public long getLastMillisecond(Calendar calendar) {
+		return getLastMillisecond();
+	}
+}
--- src/main/java/org/psicover/altimeter/AltimeterVisualization.java
+++ src/main/java/org/psicover/altimeter/AltimeterVisualization.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
 import java.util.List;
 import java.util.Locale;
 
@@ -20,8 +22,12 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.apache.batik.anim.dom.SVGDOMImplementation;
@@ -33,14 +39,23 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartMouseEvent;
+import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.entity.ChartEntity;
+import org.jfree.chart.entity.LegendItemEntity;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.data.time.DateRange;
+import org.jfree.data.time.RegularTimePeriod;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
 import org.odftoolkit.simple.SpreadsheetDocument;
 import org.odftoolkit.simple.table.Table;
 import org.w3c.dom.DOMImplementation;
@@ -147,8 +162,9 @@ public class AltimeterVisualization extends JFrame {
 	}
 
 	private void doExportCSV() {
-		ChartPanel panel = (ChartPanel)pane.getSelectedComponent();
+		JPanel panel = (JPanel)pane.getSelectedComponent();
 		if(panel == null) return;
+		
 		JFileChooser jfc = new JFileChooser(lastDirectory);
 		jfc.addChoosableFileFilter(new FileNameExtensionFilter("Tab Delimited Values (*.tsv, *.tab)", "tsv", "tab"));
 		jfc.addChoosableFileFilter(new FileNameExtensionFilter("Comma Delimited Values (*.csv)", "csv"));
@@ -192,8 +208,9 @@ public class AltimeterVisualization extends JFrame {
 				csAlt.setDataFormat((short)4); // "#,##0.00"
 				
 				for(int tab = 0; tab < pane.getTabCount(); tab++) {
-					ChartPanel panel = (ChartPanel)pane.getComponentAt(tab);//TabComponentAt(tab);
+					JPanel panel = (JPanel)pane.getComponentAt(tab);//TabComponentAt(tab);
 					if(panel == null) continue;
+
 					// String title = pane.getTitleAt(tab); 
 					String title = String.format("Session %d", tab+1);
 					AltimeterSession session = (AltimeterSession) panel.getClientProperty("raw session");
@@ -264,15 +281,16 @@ public class AltimeterVisualization extends JFrame {
 	}
 	
 	private void doExportPNG() {
-		ChartPanel panel = (ChartPanel)pane.getSelectedComponent();
+		JPanel panel = (JPanel)pane.getSelectedComponent();
 		if(panel == null) return;
+		ChartPanel chartPanel = (ChartPanel) panel.getComponent(0);
 		JFileChooser jfc = new JFileChooser(lastDirectory);
 		jfc.addChoosableFileFilter(new FileNameExtensionFilter("PNG image (*.png)", "png"));
 		if(jfc.showSaveDialog(this)==JFileChooser.APPROVE_OPTION) {
 			lastDirectory = jfc.getCurrentDirectory();
 			// TODO add extension if necessary
 			try {
-				ChartUtilities.saveChartAsPNG(jfc.getSelectedFile(), panel.getChart(),
+				ChartUtilities.saveChartAsPNG(jfc.getSelectedFile(), chartPanel.getChart(),
 						panel.getWidth(), panel.getHeight());
 			} catch (IOException e) {
 				// TODO display error message
@@ -282,15 +300,16 @@ public class AltimeterVisualization extends JFrame {
 	}
 
 	private void doExportSVG() {
-		ChartPanel panel = (ChartPanel)pane.getSelectedComponent();
+		JPanel panel = (JPanel)pane.getSelectedComponent();
 		if(null == panel) return;
+		ChartPanel chartPanel = (ChartPanel) panel.getComponent(0);
 
 		JFileChooser jfc = new JFileChooser(lastDirectory);
 		jfc.addChoosableFileFilter(new FileNameExtensionFilter("SVG image (*.svg)", "svg"));
 		if(jfc.showSaveDialog(this)==JFileChooser.APPROVE_OPTION) {
 			// TODO add extension if necessary
 			lastDirectory = jfc.getCurrentDirectory();
-			JFreeChart chart = panel.getChart();
+			JFreeChart chart = chartPanel.getChart();
 			int x = panel.getWidth();
 			int y = panel.getHeight();
 
@@ -324,27 +343,29 @@ public class AltimeterVisualization extends JFrame {
 
 		int i = 0;
 		for(AltimeterSession session : sessions) {
+			// TODO extract to custom panel.
+			
 			i++;
 			AltimeterSample [] samples = session.getData();
 			int nsamples = samples.length;
 			SampleRate rate = session.getRate();
 			int duration = rate.duration(nsamples);
-			double tuIncr = 1.0/rate.samplesPerSecond();
 			int hours = duration/3600;
 			int durm = duration%3600;
 			int minutes = durm/60;
 			int seconds = durm%60;
 			String title = String.format("Session %d - %02d:%02d:%02d", i, hours, minutes, seconds);
 			
-			// TODO adjust ticks, scales, zoom, etc...
-			XYSeriesCollection dataset = new XYSeriesCollection();
-			XYSeriesCollection tempDataSet = new XYSeriesCollection();
-			XYSeries altSeries = new XYSeries("Altitude (m)");
-			XYSeries altSSeries = new XYSeries("Smoothed Altitude (m)");
-			XYSeries tempSeries = new XYSeries("Temperature (C)");
-			XYSeries tempSSeries = new XYSeries("Smoothed Temperature (C)");
 			
-			double x = 0;
+			// TODO adjust ticks, scales, zoom, etc...
+			TimeSeriesCollection dataset = new TimeSeriesCollection();
+			TimeSeriesCollection tempDataSet = new TimeSeriesCollection();
+			TimeSeries altSeries = new TimeSeries("Altitude (m)");
+			TimeSeries altSSeries = new TimeSeries("Smoothed Altitude (m)");
+			TimeSeries tempSeries = new TimeSeries("Temperature (C)");
+			TimeSeries tempSSeries = new TimeSeries("Smoothed Temperature (C)");
+			RegularTimePeriod tp=new AltimeterTimePeriod(rate);
+			
 			double [] smoothAlt = new double[samples.length];
 			double [] smoothTem = new double[samples.length];
 			for(int j = 0; j < samples.length; j++) {
@@ -371,11 +392,11 @@ public class AltimeterVisualization extends JFrame {
 				smoothAlt[j] = sumAlt/cnt;
 				smoothTem[j] = sumTem/cnt;
 				
-				altSeries.add(x, alt);
-				altSSeries.add(x, smoothAlt[j]);
-				tempSeries.add(x, tem);
-				tempSSeries.add(x, smoothTem[j]);
-				x+=tuIncr;
+				altSeries.add(tp, alt);
+				altSSeries.add(tp, smoothAlt[j]);
+				tempSeries.add(tp, tem);
+				tempSSeries.add(tp, smoothTem[j]);
+				tp = tp.next();
 			}
 			
 			dataset.addSeries(altSeries);
@@ -384,15 +405,33 @@ public class AltimeterVisualization extends JFrame {
 			tempDataSet.addSeries(tempSSeries);
 			
 			JFreeChart chart = ChartFactory.createXYLineChart(title,
-		            "Time", "Altitude", dataset);
+		            "Time (s)", "Altitude (m)", dataset);
 			
 	        final XYPlot plot = chart.getXYPlot();
-	        final NumberAxis axis2 = new NumberAxis("Temperature");
+	        final NumberAxis axis2 = new NumberAxis("Temperature (C)");
 	        axis2.setAutoRangeIncludesZero(true);
 	        plot.setRangeAxis(1, axis2);
 	        plot.setDataset(1, tempDataSet);
 	        plot.mapDatasetToRangeAxis(1, 1);
 	        ((NumberAxis)plot.getRangeAxis()).setAutoRangeIncludesZero(false);
+	        
+	        final NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+	        domainAxis.setNumberFormatOverride(new DecimalFormat("##0") {
+				private static final long serialVersionUID = 9200880572231520816L;
+				@Override
+	        	public StringBuffer format(double number, StringBuffer result, FieldPosition fieldPosition) {
+	        		// TODO Auto-generated method stub
+	        		return super.format(number/1000, result, fieldPosition);
+	        	}
+	        	@Override
+	        	public StringBuffer format(long number, StringBuffer result, FieldPosition fieldPosition) {
+	        		// TODO Auto-generated method stub
+	        		return super.format(number/1000L, result, fieldPosition);
+	        	}
+	        });
+//	        
+//			DateAxis axis = (DateAxis) plot.getDomainAxis();
+//			axis.setDateFormatOverride(new SimpleDateFormat(dateFormat));
 	        
 			
 	        final StandardXYItemRenderer renderer2 = new StandardXYItemRenderer();
@@ -401,10 +440,59 @@ public class AltimeterVisualization extends JFrame {
 	        // renderer.setToolTipGenerator(StandardXYToolTipGenerator.getTimeSeriesInstance());
 	        plot.setRenderer(1, renderer2);
 			
-			
 			ChartPanel chartPanel = new ChartPanel(chart);
-			chartPanel.putClientProperty("raw session", session);
-			pane.add(title, chartPanel);
+			JPanel panel = new JPanel();
+			panel.setLayout(new BorderLayout());
+			panel.add(chartPanel, BorderLayout.CENTER);
+			panel.putClientProperty("raw session", session);
+			
+			// TODO input to set zoomLevel
+			final int stepSize = 100;
+			
+			// setup dos botoes
+			final JSlider slider = new JSlider(0, (int)(tp.getFirstMillisecond()/1000L), 0);
+			panel.add(slider, BorderLayout.SOUTH);
+			slider.addChangeListener(new ChangeListener() {
+				
+				@Override
+				public void stateChanged(ChangeEvent e) {
+		            int value = slider.getValue();
+		            DateRange range = new DateRange(value*1000,(value+stepSize)*1000);
+		            domainAxis.setRange(range);
+				}
+			});
+            DateRange range = new DateRange(0,stepSize*1000);
+            domainAxis.setRange(range);
+
+            // http://stackoverflow.com/questions/24562775/setting-series-visiblity-to-false-also-hides-it-from-the-legend
+            // chartPanel.addChartMouseListener(new ChartMouseListener() {
+			// 	
+			// 	@Override
+			// 	public void chartMouseMoved(ChartMouseEvent event) {
+			// 		// TODO Auto-generated method stub
+			// 		
+			// 	}
+			// 	
+            //     @Override
+            //     public void chartMouseClicked(ChartMouseEvent event) {
+            //         ChartEntity entity = event.getEntity();
+            //         if (entity instanceof LegendItemEntity) {
+            //             //*
+            //             LegendItemEntity itemEntity = (LegendItemEntity) entity;
+            //             XYDataset dataset = (XYDataset) itemEntity.getDataset();
+            //             int index = dataset.indexOf(itemEntity.getSeriesKey());
+            //             XYPlot plot = (XYPlot) event.getChart().getPlot();
+            // 
+            //             //set the renderer to hide the series
+            //             XYItemRenderer renderer = plot.getRenderer();
+            //             renderer.setSeriesVisible(index, !renderer.isSeriesVisible(index), false);
+            //             renderer.setSeriesVisibleInLegend(index, true, false);
+            //             //*/        
+            //         }
+            //     }
+            // });            
+            
+			pane.add(title, panel);
 		}
 	}
 
