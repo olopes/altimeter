e4581938960e1744a3413698a809806faff6b3bc
olopes <psicover.dev@gmail.com>
2016-12-29 01:04:15 +0000

    Convert altimeter files from command line

--- src/main/java/org/psicover/altimeter/AltimeterUtil.java
+++ src/main/java/org/psicover/altimeter/AltimeterUtil.java
@@ -1,15 +1,83 @@
 package org.psicover.altimeter;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.psicover.altimeter.ui.AltimeterVisualization;
 
 public class AltimeterUtil {
+	
+	public static void printUsage(Options options) {
+		System.out.println("usage: AltimeterUtil [FILE]");
+		System.out.println("       AltimeterUtil -c [-f <format>] [-n <session-number>] -o <OUTFILE> [FILE]");
+		System.out.println();
+		System.out.println("If no options are given, opens FILE in data visualization GUI");
+		System.out.println("Otherwise performs file convertion");
+
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp( "AltimeterUtil", options, true );
+	}
 
 	public static void main(String[] args) {
-		// TODO parse arguments
-		if(args.length == 0)
-			AltimeterVisualization.main(args);
-		else
-			ExportData.main(args);
+		// create Options object
+		Options options = new Options();
+
+		// add t option
+		options.addOption("h","help", false, "display this help message");
+		options.addOption("c", "convert", false, "convert file");
+		options.addOption("f", "format", true, "output format, use 'list' to show all available formats");
+		options.addOption("n", "session-number", true, "session number to convert, defaults to 0");
+		options.addOption("o", "output", true, "output file");
+		
+		DefaultParser parser = new DefaultParser();
+		CommandLine cmd = null;		
+		
+	    try {
+	        // parse the command line arguments
+	    	cmd = parser.parse( options, args );
+	    }
+	    catch( ParseException exp ) {
+	        // oops, something went wrong
+	        System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
+	        
+    		printUsage(options);
+	        return;
+	    }
+	    
+	    String [] otherArgs = cmd.getArgs();
+
+	    String format = cmd.getOptionValue("format", "xlsx");
+	    String outFile = cmd.getOptionValue("output");
+	    String sessionNum = cmd.getOptionValue("session-number", "0");
+	    
+	    
+	    
+	    
+	    // work with arguments
+	    if(cmd.hasOption("format") && "list".equals(cmd.getOptionValue("format"))) {
+	    	ExportData.printFormats();
+	    }
+	    
+	    if(cmd.hasOption("convert")) {
+	    	if(otherArgs.length!=1) {
+	    		System.out.println("No input file given.");
+	    		System.out.println();
+	    		printUsage(options);
+	    		return;
+	    	}
+	    	if(!cmd.hasOption("output")) {
+	    		System.out.println("No output file given.");
+	    		System.out.println();
+	    		printUsage(options);
+	    		return;
+	    	}
+			ExportData.export(format, otherArgs[0], sessionNum, outFile);
+	    } else {
+			AltimeterVisualization.main(otherArgs);
+	    }
+		
 	}
 
 }
--- src/main/java/org/psicover/altimeter/ExportData.java
+++ src/main/java/org/psicover/altimeter/ExportData.java
@@ -1,10 +1,84 @@
 package org.psicover.altimeter;
 
+import java.io.File;
+
+import org.psicover.altimeter.bean.AltimeterFile;
+import org.psicover.altimeter.bean.AltimeterSession;
+import org.psicover.altimeter.io.AltimeterFileReader;
+import org.psicover.altimeter.io.DlmFileWriter;
+import org.psicover.altimeter.io.IExportDataAdapter;
+import org.psicover.altimeter.io.OdsFileWriter;
+import org.psicover.altimeter.io.XlsxFileWriter;
+import org.psicover.altimeter.ui.AltimeterIOException;
+
 public class ExportData {
+	
+	public static enum OutputFormat {
+		xlsx(XlsxFileWriter.getInstance(),"Excel spreadsheet (default)"),
+		ods(OdsFileWriter.getInstance(),"ODF Spreadsheet"),
+		tsv(DlmFileWriter.getTsvInstance(),"Tab Delimited Values - '\\t'"),
+		tab(DlmFileWriter.getTsvInstance(),"Tab Delimited Values - '\\t'"),
+		csv(DlmFileWriter.getCsvInstance(),"Comma Delimited Values - ','"),
+		ssv(DlmFileWriter.getSsvInstance(),"Semicolon Delimited Values - ';'"),
+;
+		private final IExportDataAdapter adapter;
+		private final String description;
+		private OutputFormat(IExportDataAdapter adapter, String description) {
+			this.description=description;
+			this.adapter=adapter;
+		}
+		public IExportDataAdapter getAdapter() {
+			return adapter;
+		}
+		
+		public String getDescription() {
+			return description;
+		}
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void printFormats() {
+		System.out.println("Available formats:");
+		for(OutputFormat f : OutputFormat.values())
+			System.out.println("  "+f.name()+" - "+f.getDescription());
+	}
+	
+	public static void export(String format, String inFile, String sessionNum, String outFile) {
+		OutputFormat fmt = null;
+		try {
+			fmt = OutputFormat.valueOf(format);
+		} catch(Throwable t) {
+			System.out.println("Unknown format '"+fmt+"'");
+			printFormats();
+			return;
+		}
+		
+		File in = new File(inFile);
+		if(!(in.exists() && in.canRead() && in.isFile())) {
+			System.out.println("Input file not found '"+inFile+"'");
+			return;
+		}
+		
+		AltimeterFile data = null;
+		try {
+			data = AltimeterFileReader.readFile(in);
+		} catch (AltimeterIOException e) {
+			System.out.println("Error reading input file: "+e.getMessage());
+			return;
+		}
+		
+		File ou = new File(outFile);
 
+		try {
+			fmt.getAdapter().write(data, data.getSessions()[Integer.parseInt(sessionNum)], ou);
+		} catch (AltimeterIOException e) {
+			System.out.println("Error writing to output file: "+e.getMessage());
+		} catch(NumberFormatException | ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid session number: "+sessionNum);
+			System.out.println("List of available sessions found:");
+			AltimeterSession [] sessions = data.getSessions();
+			for(int i = 0; i < sessions.length; i++)
+				System.out.println("  Session "+i+" - duration "+sessions[i].getSessionDuration());
+		}
 	}
 
 }
