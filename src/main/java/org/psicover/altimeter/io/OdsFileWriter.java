27d86fddfc289f740ddfa0c823ca290861e785a6
olopes <psicover.dev@gmail.com>
2017-01-17 01:09:22 +0000

    Removed dependency on apache poi and jOpenDocument
    Stream data directly to file to speed things up and  more memory
    efficient.

--- src/main/java/org/psicover/altimeter/io/OdsFileWriter.java
+++ src/main/java/org/psicover/altimeter/io/OdsFileWriter.java
@@ -1,15 +1,17 @@
 package org.psicover.altimeter.io;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import javax.swing.table.TableModel;
-
-import org.jopendocument.dom.spreadsheet.Sheet;
-import org.jopendocument.dom.spreadsheet.SpreadSheet;
 import org.psicover.altimeter.bean.AltimeterFile;
+import org.psicover.altimeter.bean.AltimeterSample;
 import org.psicover.altimeter.bean.AltimeterSession;
-import org.psicover.altimeter.bean.AltimeterTableModel;
 
 public class OdsFileWriter implements IExportDataAdapter {
 
@@ -23,24 +25,72 @@ public class OdsFileWriter implements IExportDataAdapter {
 	}
 	
 	// FIXME it has a lot of performance issues. Maybe a streaming solution like SXSSF
-
 	public void write(AltimeterFile data, AltimeterSession ignore, File outputFile) throws AltimeterIOException {
+		ZipOutputStream zout = null;
+		PrintStream ps = null;
 		try {
-			// Save the data to an ODS file and open it.
-			AltimeterSession[] sessions = data.getSessions();
-			SpreadSheet spreadsheet = SpreadSheet.create(sessions.length, 1, 1);
+			DecimalFormat fmtTime = new DecimalFormat("0.0##", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+			DecimalFormat fmtInt = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+			DecimalFormat fmtAlt = new DecimalFormat("0.0#############", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
 			
-			for(int i = 0; i < sessions.length; i++) {
-				String title = String.format("Session %d", i+1);
-				TableModel tableModel = new AltimeterTableModel(sessions[i]);
-				Sheet sheet = spreadsheet.getSheet(i);
-				sheet.setName(title);
-				sheet.merge(tableModel, 0, 0, true);
+			zout = new ZipOutputStream(new FileOutputStream(outputFile));
+			ps = new PrintStream(zout, true, "UTF-8");
+			
+			zout.putNextEntry(new ZipEntry("META-INF/manifest.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			ps.println("<manifest:manifest xmlns:manifest=\"urn:oasis:names:tc:opendocument:xmlns:manifest:1.0\"><manifest:file-entry manifest:media-type=\"application/vnd.oasis.opendocument.spreadsheet\" manifest:full-path=\"/\" /><manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"meta.xml\" /><manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"content.xml\" /><manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"styles.xml\" /></manifest:manifest>");
+			ps.flush();
+			
+			zout.putNextEntry(new ZipEntry("meta.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			ps.println("<office:document-meta xmlns:office=\"urn:oasis:names:tc:opendocument:xmlns:office:1.0\" xmlns:svg=\"urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0\" xmlns:fo=\"urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0\" xmlns:draw=\"urn:oasis:names:tc:opendocument:xmlns:drawing:1.0\" xmlns:script=\"urn:oasis:names:tc:opendocument:xmlns:script:1.0\" xmlns:ooo=\"http://openoffice.org/2004/office\" xmlns:number=\"urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0\" xmlns:form=\"urn:oasis:names:tc:opendocument:xmlns:form:1.0\" xmlns:meta=\"urn:oasis:names:tc:opendocument:xmlns:meta:1.0\" xmlns:style=\"urn:oasis:names:tc:opendocument:xmlns:style:1.0\" xmlns:text=\"urn:oasis:names:tc:opendocument:xmlns:text:1.0\" xmlns:config=\"urn:oasis:names:tc:opendocument:xmlns:config:1.0\" xmlns:table=\"urn:oasis:names:tc:opendocument:xmlns:table:1.0\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" office:version=\"1.2\"><office:meta><meta:generator>Altimeter Visualization Utility</meta:generator></office:meta></office:document-meta>");
+			ps.flush();
+			
+			zout.putNextEntry(new ZipEntry("mimetype"));
+			ps.print("application/vnd.oasis.opendocument.spreadsheet");
+			ps.flush();
+			
+			zout.putNextEntry(new ZipEntry("styles.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			ps.println("<office:document-styles xmlns:office=\"urn:oasis:names:tc:opendocument:xmlns:office:1.0\" xmlns:svg=\"urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0\" xmlns:fo=\"urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0\" xmlns:draw=\"urn:oasis:names:tc:opendocument:xmlns:drawing:1.0\" xmlns:script=\"urn:oasis:names:tc:opendocument:xmlns:script:1.0\" xmlns:ooo=\"http://openoffice.org/2004/office\" xmlns:number=\"urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0\" xmlns:form=\"urn:oasis:names:tc:opendocument:xmlns:form:1.0\" xmlns:meta=\"urn:oasis:names:tc:opendocument:xmlns:meta:1.0\" xmlns:style=\"urn:oasis:names:tc:opendocument:xmlns:style:1.0\" xmlns:text=\"urn:oasis:names:tc:opendocument:xmlns:text:1.0\" xmlns:config=\"urn:oasis:names:tc:opendocument:xmlns:config:1.0\" xmlns:table=\"urn:oasis:names:tc:opendocument:xmlns:table:1.0\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" office:version=\"1.2\"><office:styles /><office:automatic-styles /><office:master-styles /></office:document-styles>");
+			ps.flush();
+
+			zout.putNextEntry(new ZipEntry("content.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			ps.print("<office:document-content xmlns:office=\"urn:oasis:names:tc:opendocument:xmlns:office:1.0\" xmlns:svg=\"urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0\" xmlns:fo=\"urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0\" xmlns:draw=\"urn:oasis:names:tc:opendocument:xmlns:drawing:1.0\" xmlns:script=\"urn:oasis:names:tc:opendocument:xmlns:script:1.0\" xmlns:ooo=\"http://openoffice.org/2004/office\" xmlns:number=\"urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0\" xmlns:form=\"urn:oasis:names:tc:opendocument:xmlns:form:1.0\" xmlns:meta=\"urn:oasis:names:tc:opendocument:xmlns:meta:1.0\" xmlns:style=\"urn:oasis:names:tc:opendocument:xmlns:style:1.0\" xmlns:text=\"urn:oasis:names:tc:opendocument:xmlns:text:1.0\" xmlns:config=\"urn:oasis:names:tc:opendocument:xmlns:config:1.0\" xmlns:table=\"urn:oasis:names:tc:opendocument:xmlns:table:1.0\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" office:version=\"1.2\">");
+			ps.print("<office:automatic-styles><style:style style:family=\"table\" style:name=\"ta0\"><style:table-properties table:display=\"true\"/></style:style></office:automatic-styles>");
+			ps.print("<office:body><office:spreadsheet>");
+			final int numSheets = data.getSessions().length;
+			// loop
+			for(int i = 0; i < numSheets; i++) {
+				AltimeterSession session = data.getSessions()[i];
+				ps.print("<table:table table:style-name=\"ta0\" table:name=\"Session "+(i+1)+"\">");
+				ps.print("<table:table-column/><table:table-column/><table:table-column/><table:table-column/>");
+				// header
+				ps.print("<table:table-row>");
+				ps.print("<table:table-cell office:value-type=\"string\"><text:p>TIME</text:p></table:table-cell>");
+				ps.print("<table:table-cell office:value-type=\"string\"><text:p>PRESSURE</text:p></table:table-cell>");
+				ps.print("<table:table-cell office:value-type=\"string\"><text:p>TEMPERATURE</text:p></table:table-cell>");
+				ps.print("<table:table-cell office:value-type=\"string\"><text:p>ALTITUDE</text:p></table:table-cell>");
+				ps.print("</table:table-row>");
+				
+				for(AltimeterSample sample : session.getData()) {
+					ps.print("<table:table-row>");
+					ps.print("<table:table-cell office:value-type=\"float\" office:value=\""+fmtTime.format(sample.getTime())+"\"><text:p>"+fmtTime.format(sample.getTime())+"</text:p></table:table-cell>");
+					ps.print("<table:table-cell office:value-type=\"float\" office:value=\""+fmtInt.format(sample.getPressure())+"\"><text:p>"+fmtInt.format(sample.getPressure())+"</text:p></table:table-cell>");
+					ps.print("<table:table-cell office:value-type=\"float\" office:value=\""+fmtInt.format(sample.getTemperature())+"\"><text:p>"+fmtInt.format(sample.getTemperature())+"</text:p></table:table-cell>");
+					ps.print("<table:table-cell office:value-type=\"float\" office:value=\""+fmtTime.format(sample.getAltitude())+"\"><text:p>"+fmtAlt.format(sample.getAltitude())+"</text:p></table:table-cell>");
+					ps.print("</table:table-row>");
+				}
+				ps.print("</table:table>");
 			}
-			
-			spreadsheet.saveAs(outputFile);
-		} catch (IOException e) {
-			throw new AltimeterIOException(e);
+			ps.println("</office:spreadsheet></office:body></office:document-content>");
+			ps.flush();
+		} catch(Throwable t) {
+			throw new AltimeterIOException(t);
+		} finally {
+			if(ps != null)
+				ps.close();
 		}
 	}
 
--- src/main/java/org/psicover/altimeter/io/XlsxFileWriter.java
+++ src/main/java/org/psicover/altimeter/io/XlsxFileWriter.java
@@ -2,14 +2,16 @@ package org.psicover.altimeter.io;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.PrintStream;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.DataFormat;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.streaming.SXSSFSheet;
-import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.psicover.altimeter.bean.AltimeterFile;
 import org.psicover.altimeter.bean.AltimeterSample;
 import org.psicover.altimeter.bean.AltimeterSession;
@@ -25,66 +27,124 @@ public class XlsxFileWriter implements IExportDataAdapter {
 	}
 
 	public void write(AltimeterFile data, AltimeterSession ignore, File selectedFile) throws AltimeterIOException {
-		try(SXSSFWorkbook wb = new SXSSFWorkbook(100)) { // keep 100 rows in memory, exceeding rows will be flushed to disk
-			// wb.setCompressTempFiles(true); // temp files will be gzipped
-			try (FileOutputStream fout = new FileOutputStream(selectedFile)){
-				CellStyle csTime = wb.createCellStyle();
-				DataFormat dfTime = wb.createDataFormat();
-				csTime.setDataFormat(dfTime.getFormat("#,##0.000"));
-				CellStyle csInt = wb.createCellStyle();
-				csInt.setDataFormat((short)3); // "#,##0"
-				CellStyle csAlt = wb.createCellStyle();
-				csAlt.setDataFormat((short)4); // "#,##0.00"
+		ZipOutputStream zout = null;
+		PrintStream ps = null;
+		try {
+			DecimalFormat fmtTime = new DecimalFormat("0.0##", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+			DecimalFormat fmtInt = new DecimalFormat("0.0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+			DecimalFormat fmtAlt = new DecimalFormat("0.0##############", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+			DateFormat tsFmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+			
+			final int numSessions = data.getSessions().length;
+			zout = new ZipOutputStream(new FileOutputStream(selectedFile));
+			ps = new PrintStream(zout, true, "UTF-8");
+			
+			zout.putNextEntry(new ZipEntry("_rels/.rels"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+			ps.println("<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\">");
+			ps.println("<Relationship Id=\"rId1\" Target=\"xl/workbook.xml\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument\"/>");
+			ps.println("<Relationship Id=\"rId2\" Target=\"docProps/app.xml\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties\"/>");
+			ps.println("<Relationship Id=\"rId3\" Target=\"docProps/core.xml\" Type=\"http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties\"/>");
+			ps.println("</Relationships>");
+			ps.flush();
 
-				AltimeterSession[] sessions = data.getSessions();
+			zout.putNextEntry(new ZipEntry("[Content_Types].xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+			ps.println("<Types xmlns=\"http://schemas.openxmlformats.org/package/2006/content-types\">");
+			ps.println("<Default ContentType=\"application/vnd.openxmlformats-package.relationships+xml\" Extension=\"rels\"/>");
+			ps.println("<Default ContentType=\"application/xml\" Extension=\"xml\"/>");
+			ps.println("<Override ContentType=\"application/vnd.openxmlformats-officedocument.extended-properties+xml\" PartName=\"/docProps/app.xml\"/>");
+			ps.println("<Override ContentType=\"application/vnd.openxmlformats-package.core-properties+xml\" PartName=\"/docProps/core.xml\"/>");
+			ps.println("<Override ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml\" PartName=\"/xl/sharedStrings.xml\"/>");
+			ps.println("<Override ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml\" PartName=\"/xl/styles.xml\"/>");
+			ps.println("<Override ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml\" PartName=\"/xl/workbook.xml\"/>");
+			// loop
+			for(int i = 0; i < numSessions; i++)
+				ps.println("<Override ContentType=\"application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml\" PartName=\"/xl/worksheets/sheet"+(i+1)+".xml\"/>");
+			ps.println("</Types>");
+			ps.flush();
+			
+			
+			zout.putNextEntry(new ZipEntry("docProps/app.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			ps.print("<Properties xmlns=\"http://schemas.openxmlformats.org/officeDocument/2006/extended-properties\"><Application>Altimeter Visualization Tool</Application></Properties>");
+			ps.flush();
+			
+			zout.putNextEntry(new ZipEntry("docProps/core.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+			ps.println("<cp:coreProperties xmlns:cp=\"http://schemas.openxmlformats.org/package/2006/metadata/core-properties\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">");
+			ps.println("<dcterms:created xsi:type=\"dcterms:W3CDTF\">"+tsFmt.format(new Date())+"</dcterms:created>");
+			ps.println("<dc:creator>Altimeter Visualization Tool</dc:creator>");
+			ps.println("</cp:coreProperties>");
+			ps.flush();
+			
+			zout.putNextEntry(new ZipEntry("xl/sharedStrings.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			ps.print("<sst count=\"0\" uniqueCount=\"0\" xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\"/>");
+			ps.flush();
+			
+			zout.putNextEntry(new ZipEntry("xl/styles.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			ps.print("<styleSheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\"><numFmts count=\"1\"><numFmt numFmtId=\"164\" formatCode=\"#,##0.000\"/></numFmts><fonts count=\"1\"><font><sz val=\"11.0\"/><color indexed=\"8\"/><name val=\"Calibri\"/><family val=\"2\"/><scheme val=\"minor\"/></font></fonts><fills count=\"2\"><fill><patternFill patternType=\"none\"/></fill><fill><patternFill patternType=\"darkGray\"/></fill></fills><borders count=\"1\"><border><left/><right/><top/><bottom/><diagonal/></border></borders><cellStyleXfs count=\"1\"><xf numFmtId=\"0\" fontId=\"0\" fillId=\"0\" borderId=\"0\"/></cellStyleXfs><cellXfs count=\"4\"><xf numFmtId=\"0\" fontId=\"0\" fillId=\"0\" borderId=\"0\" xfId=\"0\"/><xf numFmtId=\"164\" fontId=\"0\" fillId=\"0\" borderId=\"0\" xfId=\"0\" applyNumberFormat=\"true\"/><xf numFmtId=\"3\" fontId=\"0\" fillId=\"0\" borderId=\"0\" xfId=\"0\" applyNumberFormat=\"true\"/><xf numFmtId=\"4\" fontId=\"0\" fillId=\"0\" borderId=\"0\" xfId=\"0\" applyNumberFormat=\"true\"/></cellXfs></styleSheet>");
+			ps.flush();
+			
+			zout.putNextEntry(new ZipEntry("xl/workbook.xml"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			ps.print("<workbook xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\">");
+			ps.print("<workbookPr date1904=\"false\"/>");
+			ps.print("<bookViews>");
+			ps.print("<workbookView activeTab=\"0\"/>");
+			ps.print("</bookViews>");
+			ps.print("<sheets>");
+			for(int i = 0; i < numSessions; i++)
+				ps.print("<sheet name=\"Session "+(i+1)+"\" r:id=\"rId"+(i+3)+"\" sheetId=\""+(i+1)+"\"/>");
+			ps.print("</sheets></workbook>");
+			ps.flush();
+			
+			zout.putNextEntry(new ZipEntry("xl/_rels/workbook.xml.rels"));
+			ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+			ps.println("<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\">");
+			ps.println("<Relationship Id=\"rId1\" Target=\"sharedStrings.xml\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings\"/>");
+			ps.println("<Relationship Id=\"rId2\" Target=\"styles.xml\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles\"/>");
+			for(int i = 0; i < numSessions; i++)
+				ps.println("<Relationship Id=\"rId"+(i+3)+"\" Target=\"worksheets/sheet"+(i+1)+".xml\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet\"/>");
+			ps.println("</Relationships>");
+			ps.flush();
 
-				for(int tab = 0; tab < sessions.length; tab++) {
-					// String title = pane.getTitleAt(tab); 
-					String title = String.format("Session %d", tab+1);
-					AltimeterSession session = sessions[tab];
-					Row r;
-					Cell c;
-					SXSSFSheet sheet = wb.createSheet(title);
-					sheet.setDefaultColumnStyle(0, csTime);
-					sheet.setDefaultColumnStyle(1, csInt);
-					sheet.setDefaultColumnStyle(2, csInt);
-					sheet.setDefaultColumnStyle(3, csAlt);
-					r = sheet.createRow(0);
-					c = r.createCell(0); c.setCellValue("TIME");
-					c = r.createCell(1); c.setCellValue("PRESSURE");
-					c = r.createCell(2); c.setCellValue("TEMPERATURE");
-					c = r.createCell(3); c.setCellValue("ALTITUDE");
-					double tuIncr = 1.0/session.getRate().samplesPerSecond(); // millis per sample
+			AltimeterSession [] sessions = data.getSessions();
+			for(int i = 0; i < numSessions; i++){
+				AltimeterSession session = sessions[i];
+				zout.putNextEntry(new ZipEntry("xl/worksheets/sheet"+(i+1)+".xml"));
+				
+				// write header
+				ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+				ps.print("<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\">");
+				ps.print("<dimension ref=\"A1\"/><sheetViews><sheetView workbookViewId=\"0\""+(i==0?" tabSelected=\"true\"":"")+"/></sheetViews><sheetFormatPr defaultRowHeight=\"15.0\"/>");
+				ps.print("<cols><col min=\"1\" max=\"1\" style=\"1\" width=\"8.0\" customWidth=\"false\"/><col min=\"2\" max=\"2\" style=\"2\" width=\"8.0\" customWidth=\"false\"/><col min=\"3\" max=\"3\" style=\"2\" width=\"8.0\" customWidth=\"false\"/><col min=\"4\" max=\"4\" style=\"3\" width=\"8.0\" customWidth=\"false\"/></cols>");
+				ps.print("<sheetData>\n");
+				ps.print("<row r=\"1\">\n");
+				ps.print("<c r=\"A1\" t=\"inlineStr\"><is><t>TIME</t></is></c><c r=\"B1\" t=\"inlineStr\"><is><t>PRESSURE</t></is></c><c r=\"C1\" t=\"inlineStr\"><is><t>TEMPERATURE</t></is></c><c r=\"D1\" t=\"inlineStr\"><is><t>ALTITUDE</t></is></c></row>\n");
 
-					double time = 0;
-					int ri = 1;
-					for(AltimeterSample sample : session.getData()) {
-						r = sheet.createRow(ri);
-						c = r.createCell(0); c.setCellValue(time);
-						c = r.createCell(1); c.setCellValue(sample.getPressure());
-						c = r.createCell(2); c.setCellValue(sample.getTemperature());
-						c = r.createCell(3); c.setCellValue(sample.getAltitude());
-						ri++;
-						time += tuIncr;
-					}
+				int r = 2;
+				for(AltimeterSample sample : session.getData()) {
+					ps.print("<row r=\""+r+"\">\n");
+					ps.print("<c r=\"A"+r+"\" t=\"n\"><v>"+fmtTime.format(sample.getTime())+"</v></c>");
+					ps.print("<c r=\"B"+r+"\" t=\"n\"><v>"+fmtInt.format(sample.getPressure())+"</v></c>");
+					ps.print("<c r=\"C"+r+"\" t=\"n\"><v>"+fmtInt.format(sample.getTemperature())+"</v></c>");
+					ps.print("<c r=\"D"+r+"\" t=\"n\"><v>"+fmtAlt.format(sample.getAltitude())+"</v></c>");
+					ps.print("</row>\n");
+					r++;
 				}
-
-				wb.write(fout);
-			} catch (IOException e) {
-				throw new AltimeterIOException(e);
-			} finally {
-				wb.dispose();
-				// try {
-				// 	wb.close();
-				// } catch (IOException e) {
-				// }
+				ps.print("</sheetData><pageMargins bottom=\"0.75\" footer=\"0.3\" header=\"0.3\" left=\"0.7\" right=\"0.7\" top=\"0.75\"/></worksheet>");
+				ps.flush();
 			}
-		} catch (AltimeterIOException e) {
-			throw e;
-		} catch (IOException e) {
-			throw new AltimeterIOException(e);
+			
+		} catch (Throwable t) {
+			throw new AltimeterIOException(t);
+		} finally {
+			if(ps != null)
+				ps.close();
 		}
-
 	}
-
+	
 }
